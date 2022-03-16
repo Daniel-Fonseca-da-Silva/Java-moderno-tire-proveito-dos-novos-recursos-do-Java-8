65c3fb49e8ce6739ed5c0ca4fd3ce6e3e0aab09b
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-03-16 14:02:14 -0300

    Que venham os lambdas!

--- src/br/com/alura/OrdenaStrings.java
+++ src/br/com/alura/OrdenaStrings.java
@@ -1,7 +1,6 @@
 package br.com.alura;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
@@ -14,40 +13,26 @@ public class OrdenaStrings {
         palavras.add("Digital Innovation one");
         palavras.add("Udemy");
 
-        Comparator<String> comparador = new ComparadorPorTamanho();
-        palavras.sort(comparador);
+//        palavras.sort((s1, s2) -> {
+//                if(s1.length() < s2.length()) {
+//                    return -1;
+//                }
+//                if(s1.length() > s1.length()) {
+//                    return 1;
+//                }
+//                return 0;
+//            });
+
+        palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+
         System.out.println(palavras);
-//
-//        for (String p : palavras) {
-//            System.out.println(p);
-//        }
 
-        Consumer<String> consumidor = new ImprimeNaLinha();
+        Consumer<String> impressor = s -> System.out.println(s);
+        palavras.forEach(impressor);
 
-        palavras.forEach(consumidor);
+        palavras.forEach(s -> System.out.println(s));
 
     }
 
 }
 
-class ImprimeNaLinha implements Consumer<String>{
-
-    @Override
-    public void accept(String s) {
-        System.out.println(s);
-    }
-}
-
-class ComparadorPorTamanho implements Comparator<String> {
-
-    @Override
-    public int compare(String s1, String s2) {
-        if(s1.length() < s2.length()) {
-            return -1;
-        }
-        if(s1.length() > s1.length()) {
-            return 1;
-        }
-        return 0;
-    }
-}
